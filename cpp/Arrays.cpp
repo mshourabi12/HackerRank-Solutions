2950e1e89ff1128789fefb716f29ffb8107c5c14
Mahdi Shourabi <m.shourabi12@gmail.com>
2022-04-18 21:03:56 +0430

    correcting some issues

--- cpp/Arrays.cpp
+++ cpp/Arrays.cpp
@@ -10,19 +10,22 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n;
     
-    cin >> n;
+    scanf("%d", &n);
     
     int  a[n];
     
     for(int i = 0 ; i < n; i++)
     {
-        scanf("%d ", &a[i]);
+        scanf("%d", &a[i]);
     }
+
     
     for (int i = n - 1 ; i >= 0; i--)
     {
         printf("%d ", a[i]);
     }
+    
+    printf("\n");
     
     return 0;
 }
